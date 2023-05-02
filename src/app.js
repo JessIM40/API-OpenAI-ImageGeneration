9537d22e08038561003239c5baf246cc0ba6dc29
Jess Inga Mayhua <jessinma40@gmail.com>
2023-05-02 16:32:45 -0500

    Funcionalidad concluida: Generador de imagenes

---
+++ src/app.js
@@ -0,0 +1,58 @@
+import API_KEY from './fconfig.js'
+
+// async function fetchImages() {
+// 	const response = await fetch("https://api.openai.com/v1/images/generations", {
+// 		method: "POST",
+// 		headers: {
+// 			Authorization: `Bearer ${API_KEY}`,
+// 			"Content-Type": "application/json"
+// 		},
+// 		body: JSON.stringify({
+// 			prompt: "a polar bear on the beach",
+// 			n: 2,
+// 			size: "1024x1024"
+// 		})
+// 	})
+// 	const data = await response.json()
+// 	console.log(data)
+// }
+
+// fetchImages()
+
+// export default fetchImages;
+
+const submitIcon = document.querySelector("#submit-icon");
+const inputElement = document.querySelector("input");
+const imageSection = document.querySelector(".images-section");
+
+const getImages = async() => {
+	const options = {
+		method: "POST",
+		headers: {
+			"Authorization": `Bearer ${API_KEY}`,
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			prompt: inputElement.value,
+			n: 4,
+			size: "1024x1024"
+		})
+
+	}
+	try {
+		const response = await fetch("https://api.openai.com/v1/images/generations", options)
+		const data = await response.json();
+		data?.data.forEach(imageObject => {
+			const imageContainer = document.createElement("div");
+			imageContainer.classList.add("image-container");
+			const imageElement = document.createElement("img");
+			imageElement.setAttribute("src", imageObject.url);
+			imageContainer.append(imageElement);
+			imageSection.append(imageContainer);
+		});
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+submitIcon.addEventListener('click', getImages)
